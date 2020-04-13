a32610adcc460d29f7ef1aa1302dd1ea71dfa75b
Antonina <a.lehka@mobidev.biz>
2020-04-13 12:57:01 +0300

    added model class

---
+++ TestIOSProject/Meal.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+class Meal  {
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int){
+    
+// Initialization should fail if there is no name or if rating is negative.
+        
+        // the name must not be empty
+        guard !name.isEmpty else {
+            return nil
+            
+        }
+    
+        guard (rating>=0) && (rating<5) else {
+            return nil
+        }
+        
+        // Initialize store properties
+           self.name = name
+           self.photo = photo
+           self.rating = rating
+        }
+}
